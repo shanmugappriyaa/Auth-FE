85e75c6d465d902a35a304554b460f7aa9b79af5
shanmugappriyaa <pspri03@gmail.com>
2023-11-23 18:42:27 +0530

    netlify redirection resolved

--- src/components/OtpPage.jsx
+++ src/components/OtpPage.jsx
@@ -6,67 +6,63 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 
 function OtpPage() {
-  let navigate= useNavigate()
+  let navigate = useNavigate();
   const initialValues = {
-    Otp:"", 
+    Otp: "",
   };
   const validationSchema = Yup.object({
-    Otp:Yup.string()
-    .required("OTP should not be empty")
-    .min(6,"OTP Should be  6 char"),
+    Otp: Yup.string()
+      .required("OTP should not be empty")
+      .min(6, "OTP Should be  6 char"),
   });
   const onSubmit = async (value) => {
-    const { Otp} = value;
+    const { Otp } = value;
     const { id } = useParams();
     try {
-        let res = await AxiosService.post("/user/otp", {
-          id,
-          OTP: Otp,
-        });
-  
-        toast.success("Register Successfully");
-        navigate("/reset/"+id);
-      } catch (error) {
-        console.log("error", error);
-        toast.error("Error Occurred. Please try again later");
-      }
-    };
-    const formik = useFormik({
-      initialValues,
-      onSubmit,
-      validationSchema,
-    });
- 
-  
- 
-    
+      let res = await AxiosService.post("/user/otp", {
+        id,
+        OTP: Otp,
+      });
+
+      toast.success("Register Successfully");
+      navigate("/reset/" + id);
+    } catch (error) {
+      console.log("error", error);
+      toast.error("Error Occurred. Please try again later");
+    }
+  };
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+  });
+
   return (
     <>
       <ToastContainer position="top-right" />
-      
-        <div className="card">
+
+      <div className="card">
         <div className="card-header text-center h5 text-white bg-primary">
-           OTP Verification
+          OTP Verification
         </div>
         <div className="row d-flex  justify-content-center my-5 ">
           <div className="col col-md-5">
-          <form onSubmit={formik.handleSubmit}>
-            <div className="form-group my-2">
-                  <label className=" col-form-label">Enter Your OTP</label>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="form-group my-2">
+                <label className=" col-form-label">Enter Your OTP</label>
                 <input
-                  type="text"  className="form-control"
+                  type="text"
+                  className="form-control"
                   value={formik.values.Otp}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    name="Otp"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  name="Otp"
                 />
-                           {formik.touched.Otp && formik.errors.Otp ? (
-                    <label className="text-danger">
-                      *{formik.errors.Otp}
-                    </label>
-                  ) : (
-                    ""
-                  )}
+                {formik.touched.Otp && formik.errors.Otp ? (
+                  <label className="text-danger">*{formik.errors.Otp}</label>
+                ) : (
+                  ""
+                )}
               </div>
               <div className="form-outline mb-4">
                 <button
@@ -79,9 +75,8 @@ function OtpPage() {
               </div>
             </form>
           </div>
-          </div>
         </div>
-      
+      </div>
     </>
   );
 }
